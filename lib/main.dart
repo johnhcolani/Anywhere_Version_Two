d201b998793f88761df26871552e387bded6aada
John Colani <91166301+johnhcolani@users.noreply.github.com>
2023-08-04 13:02:58 -0400

    being portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'features/wireviewer_fearture/presentation_layer/screens/splash_screen.dart';
 
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
